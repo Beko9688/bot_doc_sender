8519b5e694361c21ed9bf317047a6fe53a9c5553
Beko9688 <alexandrov-2021@list.ru>
2022-02-09 23:45:53 +0500

    Commit to uz_3v

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -10,6 +10,6 @@ from utils.misc import rate_limit
 @rate_limit(limit=5)  # можно вызывать раз в 5 сек
 @dp.message_handler(CommandStart(), ChatTypeFilter([types.chat.ChatType.PRIVATE]))
 async def bot_start(message: types.Message):
-    await message.answer(f"Salom, {message.from_user.full_name}!\nBelgilangan chatga hujjatlarni qayta ishlash va yuborish uchun bot.\nMalumot uchun /help\n\nAgar siz allaqachon ro'yxatdan o'tgan bo'lsangiz, fayllarni to'g'ridan-to'g'ri yuborishingiz mumkin.\n\n<i>Ro'yxatdan o'tish uchun</i>/register")
+    await message.answer(f"Salom, {message.from_user.full_name}!\nBelgilangan chatga hujjatlarni qayta ishlash va yuborish uchun bot.\nMalumot uchun /help\n\nAgar siz allaqachon ro'yxatdan o'tgan bo'lsangiz, fayllarni to'g'ridan-to'g'ri yuborishingiz mumkin.\n\n<i>Ro'yxatdan o'tish uchun</i> /register")
 
 
